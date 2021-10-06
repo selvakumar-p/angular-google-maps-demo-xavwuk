5314ca3a447e6183dc561e89b6b360614e2aec43
selvakumar-p <selvakumar.p.cse@gmail.com>
2021-10-06 11:48:23 -0600

    2 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -34,19 +34,19 @@ export class AppComponent  {
 	  {
 		  lat: 51.673858,
 		  lng: 7.815982,
-		  label: '5 Min',
+		  label: '5',
 		  draggable: true
 	  },
 	  {
 		  lat: 51.373858,
 		  lng: 7.215982,
-		  label: '10 Min',
+		  label: '10',
 		  draggable: false
 	  },
 	  {
 		  lat: 51.723858,
 		  lng: 7.895982,
-		  label: '20 Min',
+		  label: '20',
 		  draggable: true
 	  }
   ]
