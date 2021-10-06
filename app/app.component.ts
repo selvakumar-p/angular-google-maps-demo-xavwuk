310516e85dcae8b853f3dd4c647cab3620461e28
selvakumar-p <selvakumar.p.cse@gmail.com>
2021-10-06 12:41:29 -0600

    2 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -34,20 +34,35 @@ export class AppComponent  {
 	  {
 		  lat: 51.673858,
 		  lng: 7.815982,
-		  label: '5',
-		  draggable: true
+		  label: {
+        color: 'red',
+        fontWeight: 'bold',
+        text: '5'
+      },
+		  draggable: true,
+      icon:'https://gist.githubusercontent.com/selvakumar-p/dcdca149d85f108ca0aa48002e25ac9a/raw/419b3a742faf333d2be030c8baf5f7c2db63b64d/marker.svg'
 	  },
 	  {
 		  lat: 51.373858,
 		  lng: 7.215982,
-		  label: '10',
-		  draggable: false
+		  label: {
+        color: 'blue',
+        fontWeight: 'bold',
+        text: '1'
+      },
+		  draggable: false,
+      icon:'https://gist.githubusercontent.com/selvakumar-p/dcdca149d85f108ca0aa48002e25ac9a/raw/419b3a742faf333d2be030c8baf5f7c2db63b64d/marker.svg'
 	  },
 	  {
 		  lat: 51.723858,
 		  lng: 7.895982,
-		  label: '20',
-		  draggable: true
+		  label: {
+        color: 'green',
+        fontWeight: 'bold',
+        text: '2'
+      },
+		  draggable: true,
+      icon:'https://gist.githubusercontent.com/selvakumar-p/cc23d282c5de644c932a5c0b3c92f132/raw/f07a4a82277626da3f250018f94455911812ad23/marker.svg'
 	  }
   ]
 }
@@ -56,6 +71,7 @@ export class AppComponent  {
 interface marker {
 	lat: number;
 	lng: number;
-	label?: string;
+	label?: any;
 	draggable: boolean;
+  icon:string;
 }
